5bcc992070189d1a9de11ab10b78d711717fa142
SDN33 <s.deinegri2@gmail.com>
2025-02-10 13:35:09 +0100

    dl

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { useEffect, useCallback } from 'react';
 import { supabase } from '../lib/supabase';
 import { useAuth } from '../context/AuthContext';
 import TrendingArticles from '../components/TrendingArticles';
+import RssFeed from '../components/RssFeed';
 
 export default function Home() {
   const { user } = useAuth();
@@ -23,8 +24,16 @@ export default function Home() {
 
   return (
     <div className="max-w-6xl mx-auto px-4 sm:px-6">
-      <h1 className="text-4xl font-bold mb-16 text-center text-indigo-600">Retrouvez nos articles sur la Tech et le Monde Digital</h1>
+      <div className="flex items-center justify-center gap-4 mb-2">
+        <img src="https://res.cloudinary.com/daroyxenr/image/upload/v1739149891/07a8a9c192544fe28acd5ee09fc6c6ca-free-removebg-preview_ulqtxh.png" alt="DataBuzz Logo" className="size-20" />
+        <h1 className="text-5xl font-bold text-center text-indigo-600">DataBuzz</h1>
+      </div>
+
+      <h1 className="text-3xl font-bold mb-16 text-center text-gray-900">Retrouvez nos articles sur la Tech et le Monde Digital</h1>
+      <div className='border-b border-gray-300 my-16'></div>
       <TrendingArticles />
+      <div className='border-b border-gray-300 my-16'></div>
+      <RssFeed />
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,13 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    proxy: {
+      '/rss': {
+        target: 'https://news.google.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/rss/, ''),
+      },
+    },
+  }
 });
